fffa30496d1b993eb3ae3ac4cffadd8afffca366
Anush <movsesyan.anush@gmail.com>
2017-01-21 23:21:25 +0400

    Not complete

---
+++ Inheritance3DShapes/Inheritance3DShapes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance3DShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cone c = new Cone();
+            Console.WriteLine();
+        }
+    }
+}
